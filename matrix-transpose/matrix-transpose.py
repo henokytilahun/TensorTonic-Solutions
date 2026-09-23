cbb8ac4ed3f4048af914e9bacef4bbd8da395726
Henok Tilahun <109305666+henokytilahun@users.noreply.github.com>
2026-09-23 15:42:47 -0700

    Solve matrix-transpose

---
+++ matrix-transpose/matrix-transpose.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def matrix_transpose(A: list) -> np.ndarray:
+    """
+    Returns the transposed matrix as a NumPy array.
+    """
+    # Write code here
+    npa = np.transpose(np.array(A))
+
+    return npa
